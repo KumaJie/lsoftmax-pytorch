5b75159a1bf816522766f384ec4400dd89a5535e
kuma <675613722@qq.com>
2023-07-12 17:05:44 +0800

    feat: CIFAR10 add

--- models.py
+++ models.py
@@ -128,3 +128,85 @@ class MNISTFIG2Net(nn.Module):
         logit = self.lsoftmax_linear(input=x, target=target)
         return logit, x
 
+class CIFAR10Net(nn.Module):
+    def __init__(self, margin, device):
+        super(CIFAR10Net, self).__init__()
+        self.margin = margin
+        self.device = device
+
+        self.conv_0 = nn.Sequential(
+            nn.BatchNorm2d(3),
+            nn.Conv2d(3, 64, 3, padding=1),
+            nn.PReLU(),
+            nn.BatchNorm2d(64)
+        )
+
+        self.conv_1 = nn.Sequential(
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.PReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.PReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.PReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.PReLU(),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.conv_2 = nn.Sequential(
+            nn.Conv2d(64, 96, 3, padding=1),
+            nn.PReLU(),
+            nn.BatchNorm2d(96),
+            nn.Conv2d(96, 96, 3, padding=1),
+            nn.PReLU(),
+            nn.BatchNorm2d(96),
+            nn.Conv2d(96, 96, 3, padding=1),
+            nn.PReLU(),
+            nn.BatchNorm2d(96),
+            nn.Conv2d(96, 96, 3, padding=1),
+            nn.PReLU(),
+            nn.BatchNorm2d(96),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.conv_3 = nn.Sequential(
+            nn.Conv2d(96, 128, 3, padding=1),
+            nn.PReLU(),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, 3, padding=1),
+            nn.PReLU(),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, 3, padding=1),
+            nn.PReLU(),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, 3, padding=1),
+            nn.PReLU(),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(2048, 256),
+            nn.BatchNorm1d(256)
+        )
+
+        self.lsoftmax_linear = LSoftmaxLinear(
+            input_features=256, output_features=10, margin=margin, device=self.device)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.lsoftmax_linear.reset_parameters()
+
+    def forward(self, x, target=None):
+        x = self.conv_0(x)
+        x = self.conv_1(x)
+        x = self.conv_2(x)
+        x = self.conv_3(x)
+        x = x.view(-1, 2048)
+        x = self.fc(x)
+        logit = self.lsoftmax_linear(input=x, target=target)
+        return logit
